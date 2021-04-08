1ca25ae30c94d776f5e7d98462fe76ad10df7ac7
recep-sen <78692387+recep-sen@users.noreply.github.com>
2021-04-08 12:37:04 +0300

    changed controller
    made player controller work and added jump and subtle changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,64 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody rb = new Rigidbody();            //getting memory for component
-    public float maxSpeed; //max speed value changable in editor
-    float x, z;          //movement coordinate values
-
-    public float deceleration;
-    public float currentSpeed;
-    private bool isMoving;
-    private Vector3 moveDirection;
-    private float epsilon = 0.0001f;
-
+    private Rigidbody rb;                      // getting memory to get rigidbody component
+    public float speed = 5f;                    // speed variable editable in editor
+    public float jump = 5f;
+    public LayerMask lm;
+    public bool grounded;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();                        //referencing component
+        rb = GetComponent<Rigidbody>();                  //adding component to variable
     }
-    private void moving()
-    {
-        isMoving = true;
-        moveDirection = new Vector3(x, 0f, z);
-    }
-    private void notmoving()
-    {
-        isMoving = false;
-        moveDirection = Vector3.zero;
-    }
+
     // Update is called once per frame
     void Update()
     {
-        currentSpeed = rb.velocity.sqrMagnitude;
-        x = Input.GetAxis("Horizontal") * Time.deltaTime;           //getting keys to variables
-        z = Input.GetAxis("Vertical") * Time.deltaTime;
-        if (x + z > 0)
+        float x = Input.GetAxisRaw("Horizontal") * speed;                 //getting input
+        float y = Input.GetAxisRaw("Vertical") * speed;
+
+        grounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z), 0.4f, lm);
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-            moving();
+            rb.velocity = new Vector3(rb.velocity.x, jump, rb.velocity.y);
         }
 
-        if (isMoving)
-        {
-            if (currentSpeed < maxSpeed)
-            {
-                rb.velocity += moveDirection;
-            }
-            else
-            {
-                rb.velocity += moveDirection;
-                rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
-            }
-        }
-        else
-        {
-            if (rb.velocity.sqrMagnitude < epsilon) rb.velocity = Vector3.zero;
-            else rb.velocity = Vector3.Lerp(rb.velocity, Vector3.zero, deceleration);
-        }
-    }
-    public void SetMaxSpeed(float newSpeed)
-    {
-        float sc = currentSpeed / maxSpeed;
-        currentSpeed = sc * newSpeed;
-        maxSpeed = newSpeed;
+        Vector3 move = transform.right * x + transform.forward * y;         //calculating movement vector from input
+        Vector3 newmove = new Vector3(move.x, rb.velocity.y, move.z);
+        rb.velocity = newmove;                                              //giving vector to component so movement can be done
     }
 }
