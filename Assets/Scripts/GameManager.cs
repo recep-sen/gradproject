2b5687b772bc7405649870c1bf81f0eed76a5eed
recep-sen <78692387+recep-sen@users.noreply.github.com>
2021-04-19 18:19:06 +0300

    added a lot read desc.
    added enemy and player models , 3rd person camera , made preparations for game manager. main menu basic screen.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError($"More than one {name} detected! Critical errors might occur.");
+            return;
+        }
+
+        instance = this;
+
+        DontDestroyOnLoad(this);
+    }
+    #endregion
+
+    public GameState gameState;
+
+    void Start()
+    {
+        gameState = GameState.MainMenu;
+    }
+
+    public enum GameState
+    {
+        MainMenu,
+        Loading,
+        Gameplay,
+        GameExit
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,18 @@ public class PlayerController : MonoBehaviour
     [Header("Movement and jump variables")]     //headers are here so i wont get lost in editor
     public float speed = 5f;                    // speed variable editable in editor
     public float jump = 5f;
+    public float rtspeed = 5f;
     [Header("SELECT GROUND LAYER")]
     public LayerMask lm;                        //getting layer so we can check it in the future
     private bool grounded;                       // a bool so i can check am i in the air or not
+
+    private Transform transform;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();                  //adding component to variable
+        transform = GetComponent<Transform>();
+
     }
 
     // Update is called once per frame
@@ -22,16 +27,31 @@ public class PlayerController : MonoBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal") * speed;                 //getting input
         float y = Input.GetAxisRaw("Vertical") * speed;
-
-        grounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z), 0.4f, lm);     //checking if am i close to floor so i cant jump indefinetly
+        grounded = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 0.4f, transform.position.z), 0.4f, lm);     //checking if am i close to floor so i cant jump indefinetly
 
         if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             rb.velocity = new Vector3(rb.velocity.x, jump, rb.velocity.y);       //jumping with if statetement to check am i up in the air
         }
+        if (x > 0)
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * rtspeed);
+        }
+        else if (x < 0)
+        {
+            transform.Rotate(-Vector3.up * Time.deltaTime * rtspeed);
+        }
 
         Vector3 move = transform.right * x + transform.forward * y;         //calculating movement vector from input
         Vector3 newmove = new Vector3(move.x, rb.velocity.y, move.z);
-        rb.velocity = newmove;                                              //giving vector to component so movement can be done
+        //transform.rotation = Quaternion.LookRotation(move);
+        //Quaternion newrotate = Quaternion.Euler(0, (x * 18), 0);
+        rb.velocity = newmove;
+        //rb.rotation = Quaternion.RotateTowards(transform.rotation, newrotate, 360f);                                      //giving vector to component so movement can be done
     }
+    /*void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - 0.4f, transform.position.z), 0.4f);
+    }*/
 }
