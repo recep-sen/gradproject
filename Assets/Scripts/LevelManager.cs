2e891fc8f2e57e731dc7d6e730eaaadee14a608d
recep-sen <78692387+recep-sen@users.noreply.github.com>
2021-04-22 17:36:20 +0300

    navmesh works now.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 public class LevelManager : MonoBehaviour
 
 {
+    public NavMeshSurface[] surfaces;
+    public Transform[] objectsToRotate;
     // Start is called before the first frame update
     [Header("Generated Texture")]
     public GameObject[] Terrain1;
@@ -20,7 +23,6 @@ public class LevelManager : MonoBehaviour
     public int quantity;
     [Header("Objects In Scene")]
     public int objectcount;
-    private bool loaded = false;
     void Awake()
     {
 
@@ -35,7 +37,7 @@ public class LevelManager : MonoBehaviour
     void LevelLoading()
     {
         randomterrain = Random.Range(0, 1);
-        Instantiate(Terrain1[0], new Vector3(0, 0, 0), Quaternion.identity);
+        Instantiate(Terrain1[randomterrain], new Vector3(0, 0, 0), Quaternion.identity);
         while (objectcount < quantity)
         {
             randomobject = Random.Range(0, 1);
@@ -47,6 +49,17 @@ public class LevelManager : MonoBehaviour
                 Instantiate(object1[randomobject], new Vector3(x, 20, z), Quaternion.identity);
             }
             objectcount++;
+        }
+        surfaces[0] = Terrain1[randomterrain].GetComponent<NavMeshSurface>();
+
+        for (int j = 0; j < objectsToRotate.Length; j++)                        //loop for rotation
+        {
+            objectsToRotate[j].localRotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0));
+        }
+        for (int i = 0; i < surfaces.Length; i++)                               //building navigation
+        {
+            Debug.Log("I need to build");
+            surfaces[i].BuildNavMesh();
         }
     }
 }
--- Assets/Scripts/NavigationBaker.cs
+++ Assets/Scripts/NavigationBaker.cs
@@ -5,18 +5,10 @@ using UnityEngine.AI;
 
 public class NavigationBaker : MonoBehaviour
 {
-    public NavMeshSurface[] surfaces;                               //getting variables
-    public Transform[] objectsToRotate;
+
 
     void Start()
     {
-        for (int j = 0; j < objectsToRotate.Length; j++)                        //loop for rotation
-        {
-            objectsToRotate[j].localRotation = Quaternion.Euler(new Vector3(0, Random.Range(0, 360), 0));
-        }
-        for (int i = 0; i < surfaces.Length; i++)                               //building navigation
-        {
-            surfaces[i].BuildNavMesh();
-        }
+
     }
 }
