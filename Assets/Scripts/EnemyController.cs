e96ad389251379398068ddcc274332f58369c798
recep-sen <78692387+recep-sen@users.noreply.github.com>
2021-05-14 19:25:43 +0300

    hilarious bug

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,24 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    private GameObject[] gameobject;                                            //necessary variables to store objects
+    //necessary variables to store objects
+    public GameObject arrowprefab;
     Transform target;
-    NavMeshAgent agent;
+    public float distance;
+    GameObject arrow;
     // Start is called before the first frame update
     void Start()
     {
-        gameobject = GameObject.FindGameObjectsWithTag("Player");                   //getting player objects transform data and navmeshagent component
-        target = gameobject[0].transform;
-        agent = GetComponent<NavMeshAgent>();
+
+
+
     }
 
     // Update is called once per frame
     void Update()                                                                   //telling the ai to chase player
     {
-        agent.SetDestination(target.position);
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        distance = Vector3.Distance(transform.position, target.position);
+        if (distance <= 20f)
+        {
+            transform.LookAt(target);
+            arrow = shootarrow();
+        }
     }
-}
+    GameObject shootarrow()
+    {
+        if (arrow != null)
+        {
+            arrow.transform.position = new Vector3(transform.position.x - 0.016f, transform.position.y - 0.004f, transform.position.z + 0.01f);
+            arrow.GetComponent<Rigidbody>().velocity = transform.forward * 100f; /*shootforce*/
+            return arrow;
+        }
+        else
+        {
+            arrow = Instantiate(arrowprefab, new Vector3(transform.position.x - 0.016f, transform.position.y - 0.004f, transform.position.z + 0.01f), Quaternion.identity);
+            arrow.GetComponent<Rigidbody>().velocity = transform.forward * 100f; /*shootforce*/
+            return arrow;
+        }
+    }
+}//0 0.2 0  0.016 0.024 -0.01
